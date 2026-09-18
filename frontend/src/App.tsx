1907f795c1969026750880e9bd0dc29802b3a537
kazu ツ <70092678+kaz-uuu@users.noreply.github.com>
2026-09-18 16:02:27 +0800

    feat(ui): design system page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import { WorkbenchShell } from "./shells/WorkbenchShell";
+import { DesignView } from "./views/DesignView";
 import { HomeView } from "./workbench/HomeView";
 import { SubmissionView } from "./workbench/SubmissionView";
 import { WorkspaceView } from "./workbench/WorkspaceView";
@@ -10,6 +11,7 @@ export function App() {
       <Route element={<WorkbenchShell />}>
         <Route index element={<HomeView />} />
         <Route path="submission" element={<SubmissionView />} />
+        <Route path="design" element={<DesignView />} />
         <Route path=":subsystem" element={<WorkspaceView />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
